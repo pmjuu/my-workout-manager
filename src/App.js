80087e26285b4ca5e9b8ae37851b7b582ec630ff
pmjuu <jsi04049@gmail.com>
2023-03-02 02:41:06 +0900

    feat: create basic markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Calendar from "./components/Calendar";
+import Setting from "./components/Setting";
+import Analysis from "./components/Analysis";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
+  const [isLogined, setIsLogined] = useState(Boolean(localStorage.getItem("user")));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header onLogin={(e) => setIsLogined(e)} isLogined={isLogined} />
+      <Routes>
+        <Route path="/" element={<Calendar />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/analysis" element={<Analysis />} />
+      </Routes>
+    </Wrapper>
   );
 }
 
