7d87e9df3125f4e833c16831967ecd8af97d9210
pmjuu <jsi04049@gmail.com>
2023-03-08 11:09:47 +0900

    test: test github

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ const Wrapper = styled.div`
 
 function App() {
 
+  async function fetchGithub() {
+    try {
+      const response = fetch("https://github.com/pmjuu/my-workout-manager");
+      console.log("response...", response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  fetchGithub();
+
   return (
     <Wrapper>
       <Header />
